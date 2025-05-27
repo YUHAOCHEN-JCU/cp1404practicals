960f49a5ba11105fb9b3b4e3306badaff0403c2f
YUHAOCHEN-JCU <haochen.yu@my.jcu.edu.au>
2025-05-27 13:02:53 +0800

    Update password_stars.py
    improve function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,14 +1,28 @@
+"""
+function main()
+    password = get_password()
+    display password
+
+
+function get password()
+    get user word
+    display information
+    return user word
+
+
+main()
+"""
 def main():
+    """print "*" based on user input"""
     password = get_password()
     print(password)
 
 
 def get_password():
+    """get user input and print "*" based on user word"""
     user_word = input("Please enter word: ")
     print("*" * len(user_word))
     return user_word
 
 
 main()
-
-
