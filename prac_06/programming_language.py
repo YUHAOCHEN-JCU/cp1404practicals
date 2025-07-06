94b98bcd3e6625bdbca113edce58814c3d7b2e9a
YUHAOCHEN-JCU <haochen.yu@my.jcu.edu.au>
2025-07-06 19:47:56 +0800

    Update programming_language.py and Add docstrings
    Add docstrings

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -6,13 +6,18 @@ Actual time: [To be filled after completion]
 
 class ProgrammingLanguage:
     def __init__(self, name, typing, reflection, year):
+        """Initialise a ProgrammingLanguage instance.
+                name, typing, reflection, year
+                """
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
+        """Add is_dynamic function to judge is dynamic"""
         return self.typing == "Dynamic"
 
     def __str__(self):
+        """return string information"""
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
