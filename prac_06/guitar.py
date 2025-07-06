2300006d0df77f553a3151c92661187d1db75cac
YUHAOCHEN-JCU <haochen.yu@my.jcu.edu.au>
2025-07-06 19:52:41 +0800

    Update guitar.py
    Add docstrings

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,14 +1,22 @@
 class Guitar:
     def __init__(self, name="", year=0, cost=0):
+        """Initialise init instance.
+                name: name of guitar
+                year: year of guitar
+                cost: cost of guitar
+                """
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """Return string information"""
         return f"{self.name} ({self.year}): ${self.cost:.2f}"
 
     def get_age(self):
+        """Get guitar's age"""
         return 2022 - self.year
 
     def is_vintage(self):
+        """Add function to judge is vintage"""
         return self.get_age() >= 50
