f707ed2ef11bea213707b69af2c5bb840c305823
YUHAOCHEN-JCU <haochen.yu@my.jcu.edu.au>
2025-07-06 18:14:44 +0800

    Update languages.py
    Format the file use PEP8 and corrected some errors in languages.py

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -5,8 +5,6 @@ python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 print(python)
-print(ruby)
-print(visual_basic)
 
 # List of languages
 languages = [python, ruby, visual_basic]
