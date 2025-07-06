2ff0e361613efcdb2a433f03d84fb252193abddb
YUHAOCHEN-JCU <haochen.yu@my.jcu.edu.au>
2025-07-06 18:25:04 +0800

    Format and update file
    Format and update file

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -5,8 +5,8 @@ gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
 another_guitar = Guitar("Another Guitar", 2013, 1000.00)
 
 # Test get_age method
-print(f"Gibson L-5 CES get_age() - Expected 103. Got {gibson.get_age()}")
-print(f"Another Guitar get_age() - Expected 12. Got {another_guitar.get_age()}")
+print(f"Gibson L-5 CES get_age() - Expected 100. Got {gibson.get_age()}")
+print(f"Another Guitar get_age() - Expected 9. Got {another_guitar.get_age()}")
 
 # Test is_vintage method
 print(f"Gibson L-5 CES is_vintage() - Expected True. Got {gibson.is_vintage()}")
