f1196418b49e9e394ea8a7144c90990f8597a0e0
YUHAOCHEN-JCU <haochen.yu@my.jcu.edu.au>
2025-07-10 14:49:47 +0800

    Add comments and optimized code structure.
    Add comments and optimized code structure.

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -1,6 +1,34 @@
 """
 CP1404/CP5632 Practical - Suggested Solution
 Programming Language class with tests.
+class ProgrammingLanguage
+
+    function __init__(self, name, typing, reflection, year, pointer_arithmetic)
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
+
+    function __str__(self)
+        return name, typing, reflection, year, pointer arithmetic
+
+    function --repr__(self)
+        return name, typing, reflection, year, pointer arithmetic
+
+    function is_dynamic(self)
+        return typing == 'Dynamic'
+
+function run_tests()
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, pointer_arithmetic="No")
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991, pointer_arithmetic="No")
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, pointer_arithmetic="No")
+    languages contain ruby, python, visual_basic
+    display python
+    display "The dynamically typed languages are:"
+    repeat language in languages
+        if language is dynamic
+            display language.name
 """
 
 
