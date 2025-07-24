d8711616f74a9e92cf3c8e43a10e9eaec90fd4d0
YUHAOCHEN-JCU <haochen.yu@my.jcu.edu.au>
2025-07-25 07:51:35 +0800

    Update prac_09_feedback
    Update prac_09_feedback

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -2,7 +2,10 @@ from car import Car
 import random
 
 class UnreliableCar(Car):
+    """A subclass of Car that simulates an unreliable car, which may not always drive
+    even when requested, based on a reliability percentage."""
     def __init__(self, name, fuel, reliability):
+        """Initialize an UnreliableCar instance."""
         super().__init__(name, fuel)
         self.reliability = reliability
 
